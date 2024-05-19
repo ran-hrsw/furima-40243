6af7f70ea6b099b54fb7c1356463f8ac9212609d
ran-hrsw <rann0722@gmail.com>
2024-05-19 18:12:27 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,7 @@ class Item < ApplicationRecord
 
   # 空の投稿を保存できないようにする
   validates :image, :item_name, :item_description, :price, presence: true
-  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than: 9_999_999 }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   # ジャンルの選択が「---」の時は保存できないようにする
   validates :category_id, :status_id, :cost_id, :address_id, :transit_time_id, numericality: { other_than: 1 }
 end
