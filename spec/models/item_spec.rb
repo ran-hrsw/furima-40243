2b23e372145ef5472e8f88e07ed9519d3bbb6a71
ran-hrsw <rann0722@gmail.com>
2024-05-19 17:35:05 +0900

    修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -81,13 +81,13 @@ RSpec.describe Item, type: :model do
       it '￥10,000以上では登録できない' do
         @item.price = 999_999_9
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price must be less than 10000')
+        expect(@item.errors.full_messages).to include('Price must be less than 9999999')
       end
 
       it '半角英数字以外では登録できない' do
         @item.price = 'らら'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price can't be blank")
+        expect(@item.errors.full_messages).to include('Price is not a number')
       end
     end
   end
