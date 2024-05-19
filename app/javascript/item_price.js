e1a72d3cf43d8dab6ef4f0666a88a9278d0202b4
ran-hrsw <rann0722@gmail.com>
2024-05-19 17:09:31 +0900

    修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -4,10 +4,8 @@ priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;
 const addTaxDom = document.getElementById("add-tax-price");
  addTaxDom.innerHTML = Math.floor(inputValue*0.1)
-console.log(Math.floor(inputValue*0.1))
 const profit = document.getElementById("profit");
  profit.innerHTML = Math.floor(inputValue*0.9)
-console.log(Math.floor(inputValue*0.9))
 })
 
 
