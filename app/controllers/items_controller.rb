7907b14f0731f3400a6e39a85f2b2158a936a86f
ran-hrsw <rann0722@gmail.com>
2024-05-25 16:36:04 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:destroy]
+  before_action :move_to_index, only: [:destroy, :edit]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -27,7 +27,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path unless current_user == @item.user
   end
 
   def update
@@ -41,20 +40,20 @@ class ItemsController < ApplicationController
   def destroy
     @item.destroy
     redirect_to root_path
+  end
 
-    def move_to_index
-      redirect_to root_path unless current_user == @item.user
-    end
+  def move_to_index
+    redirect_to root_path unless current_user == @item.user
+  end
 
-    private
+  private
 
-    def item_params
-      params.require(:item).permit(:image, :price, :item_name, :item_description, :category_id, :status_id, :cost_id, :address_id,
-                                   :transit_time_id).merge(user_id: current_user.id)
-    end
+  def item_params
+    params.require(:item).permit(:image, :price, :item_name, :item_description, :category_id, :status_id, :cost_id, :address_id,
+                                 :transit_time_id).merge(user_id: current_user.id)
+  end
 
-    def set_item
-      @item = Item.find(params[:id])
-    end
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
