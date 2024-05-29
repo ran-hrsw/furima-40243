770ff244a0d3fe067c1b7a78da07934f97597c1e
ran-hrsw <rann0722@gmail.com>
2024-05-29 20:41:26 +0900

    実装

--- app/models/buying_address.rb
+++ app/models/buying_address.rb
@@ -3,19 +3,19 @@ class BuyingAddress
 
   attr_accessor :post_code, :city, :address_id, :number, :building_name, :phone_number, :user_id, :item_id
 
-  # with_options presence: true do
-  # validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: 'is invalid'}
-  # validates :user_id
-  # validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  # end
-  # validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+  with_options presence: true do
+    validates :user_id
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+  end
+  validates :address_id, numericality: { other_than: 0, message: "can't be blank" }
 
   def save
     # 購入履歴を保村し、変数Buying_historyに代入する
     buying_history = BuyingHistory.create(user_id:, item_id:)
     # 住所を保存する
     # addresses_idには、変数addressのidと指定する
-    Information.create(post_code:, city:, address_id:, number:,
+    Information.create(buying_history:, post_code:, city:, address_id:, number:,
                        building_name:, phone_number:)
+    # 学習メモ　VSCodeの新しい記述方法　[:,]
   end
 end
