aca82a14be0271f9dbd8c4e001b4d0a2fa95d4b2
ran-hrsw <rann0722@gmail.com>
2024-06-01 18:25:18 +0900

    修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,6 +1,6 @@
 const pay = () => {
   const publicKey = gon.public_key
-  const payjp = Payjp('pk_test_2c40b2c7cd44dded82c4fa8b')// PAY.JPテスト公開鍵
+  const payjp = Payjp(publicKey)// PAY.JPテスト公開鍵
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
